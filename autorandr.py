aaaaf1c70d1bbca9e048b9255f542ef4706c60df
Phillip Berndt <phillip.berndt@googlemail.com>
2018-10-03 19:01:16 +0200

    wip: Support for --fb and --dpi
    This is an attempt to piggy-back fb and dpi onto the first used output.
    Not certain whether this has catastrophic drawbacks yet. In any case,
    the dpi information should probably be obtained from the outputs, as
    those already have all the necessary information, rather than by parsing
    the xrandr output a second time. Maybe it'd be an even better idea to go
    with --fbmm instead of dpi.
    
    See #33

--- autorandr.py
+++ autorandr.py
@@ -191,6 +191,8 @@ class XrandrOutput(object):
         "gamma": "1.0:1.0:1.0",
     }
 
+    GLOBAL_OPTIONS = ("fb", "dpi")
+
     XRANDR_DEFAULTS = dict(list(XRANDR_13_DEFAULTS.items()) + list(XRANDR_12_DEFAULTS.items()))
 
     EDID_UNAVAILABLE = "--CONNECTED-BUT-EDID-UNAVAILABLE-"
@@ -213,7 +215,7 @@ class XrandrOutput(object):
         if xrandr_version() >= Version("1.2"):
             options.update(self.XRANDR_12_DEFAULTS)
         options.update(self.options)
-        return {a: b for a, b in options.items() if a not in self.ignored_options}
+        return {a: b for a, b in options.items() if a not in self.ignored_options and a not in self.GLOBAL_OPTIONS}
 
     @property
     def filtered_options(self):
@@ -478,7 +480,18 @@ def parse_xrandr_output():
     if not xrandr_output:
         raise AutorandrException("Failed to run xrandr")
 
-    # We are not interested in screens
+    # Extract fb size and dpi
+    screen_size = re.search("(?m)^Screen 0:.+current (?P<width>[0-9]+) x (?P<height>[0-9]+)", xrandr_output)
+    res_phys_pairs = re.findall("(?m)\S+ connected (?:primary)? (?P<width>[0-9]+)x(?P<height>[0-9]+)\+.+ (?P<width_mm>[0-9]+)mm x (?P<height_mm>[0-9]+)mm", xrandr_output)
+    dpis = []
+    for pair in res_phys_pairs:
+        MM_PER_INCH = 25.4
+        dpis.append(int(pair[0]) * MM_PER_INCH / int(pair[2]))
+        dpis.append(int(pair[1]) * MM_PER_INCH / int(pair[3]))
+    dpi = "%d" % (sum(dpis) // len(dpis),) if dpis else None
+    fb = "%sx%s" % (screen_size.group("width"), screen_size.group("height")) if screen_size else None
+
+    # We aren't interested in screens anymore
     xrandr_output = re.sub("(?m)^Screen [0-9].+", "", xrandr_output).strip()
 
     # Split at output boundaries and instanciate an XrandrOutput per output
@@ -493,6 +506,17 @@ def parse_xrandr_output():
         outputs[output_name] = output
         if output_modes:
             modes[output_name] = output_modes
+
+    # Piggy-back dpi and fb as option of first non-off screen
+    for output_name in outputs:
+        if "off" in outputs[output_name].options:
+            continue
+        if dpi:
+            outputs[output_name].options["dpi"] = dpi
+        if fb:
+            outputs[output_name].options["fb"] = fb
+        break
+
 
     return outputs, modes
 
@@ -691,12 +715,25 @@ def apply_configuration(new_configuration, current_configuration, dry_run=False)
     #   explicitly, so avoid it unless necessary.
     #   (See https://github.com/phillipberndt/autorandr/issues/72)
 
-    fb_dimensions = get_fb_dimensions(new_configuration)
-    try:
-        base_argv += ["--fb", "%dx%d" % fb_dimensions]
-    except:
-        # Failed to obtain frame-buffer size. Doesn't matter, xrandr will choose for the user.
-        pass
+    # Extract global options from outputs, if configured
+    global_option_values = {}
+    for output in outputs:
+        for global_option in XrandrOutput.GLOBAL_OPTIONS:
+            if global_option in new_configuration[output].options:
+                global_option_values[global_option] = new_configuration[output].options[global_option]
+
+    # Prefix global options to args, with a default for --fb
+    if "fb" in global_option_values:
+        base_argv += ["--fb", global_option_values["fb"]]
+        del global_option_values["fb"]
+    else:
+        fb_dimensions = get_fb_dimensions(new_configuration)
+        try:
+            base_argv += ["--fb", "%dx%d" % fb_dimensions]
+        except:
+            # Failed to obtain frame-buffer size. Doesn't matter, xrandr will choose for the user.
+            pass
+    map(base_argv.extend, (("--%s" % key, value) for key, value in global_option_values.items()))
 
     auxiliary_changes_pre = []
     disable_outputs = []
