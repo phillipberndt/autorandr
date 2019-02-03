6d40d3cfba4691753661c6c786759bbc6f0ac3b1
Ricky Liou <rliou92@gmail.com>
2019-02-02 17:15:24 -0800

    Add --default default
    Added in order to behave the same way as the systemd service

--- contrib/autorandr_launcher/autorandr_launcher.c
+++ contrib/autorandr_launcher/autorandr_launcher.c
@@ -38,7 +38,7 @@ static int ar_launch()
 	pid_t pid = fork();
 	if (pid == 0) {
 		static char *argv[] =
-		    { "/usr/bin/autorandr", "--change", NULL };
+		    { "/usr/bin/autorandr", "--change", "--default", "default", NULL };
 		execve(argv[0], argv, environ);
 		exit(127);
 	} else {
