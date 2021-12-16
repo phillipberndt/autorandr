phillipberndt/autorandr

commit 5072674fcf90e20f8e381d86f33038fc8f2b288b

Replace deprecated distutils with packaging.
This is a fix for #266, a DeprecationWarning about distutils package.

autorandr.py
@@ -39,9 +39,13 @@ import time
 import glob
 
 from collections import OrderedDict
-from distutils.version import LooseVersion as Version
 from functools import reduce
 from itertools import chain
+
+try:
+    from packaging.version import Version
+except ModuleNotFound:
+    from distutils.version import LooseVersion as Version
 
 if sys.version_info.major == 2:
     import ConfigParser as configparser

commit c78ca22a22eef1f77fc47a8dc3ed5dc2929d1ccc

Fix Typo: ModuleNotFound → ModuleNotFoundError
This fixes a typo introduced in #267

autorandr.py
@@ -44,7 +44,7 @@ from itertools import chain
 
 try:
     from packaging.version import Version
-except ModuleNotFound:
+except ModuleNotFoundError:
     from distutils.version import LooseVersion as Version
 
 if sys.version_info.major == 2: